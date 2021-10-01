911b7cc1615c3be27366846301934c2cdf104582
RainingMonkey <82685105+RainingMonkey@users.noreply.github.com>
2021-10-01 18:02:13 -0500

    Update script1.js

--- script1.js
+++ script1.js
@@ -7,7 +7,7 @@ ctx.fillText("Hello World", 300, 200);
 ctx.filltext='30px Tahoma'
 ctx.strokeText('This is a stroked text', 500, 250);
 const img = new Image();
-img.src = 'Images/Leaves.jpg';
+img.src = 'Leaves.jpg';
 img.onload = function() {
     ctx.drawImage(img, 500, 400)
 }
